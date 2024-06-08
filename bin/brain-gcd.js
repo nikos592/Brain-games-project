a0ef011f001f2dca6af741e8593ea4285216be99
Илья Никулин <wixestrees@gmail.com>
2024-06-08 16:36:53 +0300

    Finished step 7

---
+++ bin/brain-gcd.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node 
+import {gameBrainGcd} from '/home/nikos/frontend-project-44/src/games/brain-gcd game.js';
+gameBrainGcd();
---
+++ src/games/brain-gcd game.js
@@ -0,0 +1,23 @@
+import { gameProcess } from '/home/nikos/frontend-project-44/src/index.js';
+
+const getGreatestCommonDivisor = (number1, number2) => {
+let a = number1;
+let b = number2;
+while (a !== 0 && b !== 0) {
+	if (a > b) {a %= b;
+} else {b %= a} 
+}
+return a + b;
+}; 
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const getQuestionAndAnswer = () => {
+	const a = Math.floor(Math.random() * 100);
+	const b = Math.floor(Math.random() * 100);
+	const question = `${a} ${b}`;
+	const correctAnswer = getGreatestCommonDivisor(a, b);
+	return [question, correctAnswer];
+};
+
+export const gameBrainGcd = () => {gameProcess(getQuestionAndAnswer,description)};
