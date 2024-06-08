461ae34a1b27198065075bef67c2234436d64444
Илья Никулин <wixestrees@gmail.com>
2024-06-08 17:30:47 +0300

    Test errors fixed(4)

--- src/games/brain-calc game.js
+++ src/games/brain-calc game.js
@@ -1,21 +1,34 @@
 import { gameProcess } from '../index.js';
-import { evaluate } from 'mathjs';
 
-const getRandomExpression = () => {
-	const operand = ['+', '-', '*'];
-	const operator1 = Math.floor(Math.random() * 100);
-	const operator2 = Math.floor(Math.random() * 100);
-	const randomOperand = operand[Math.floor(Math.random() * operand.length)];
-    const result = `${operator1} ${randomOperand} ${operator2}`;
-    return result;
-};
 
 const description = 'What is the result of the expression?';
+const operators = ['+', '-', '*'];
+const minRange = 0;
+const maxRange = 50;
+
+const calculate = (x, y, operator) => {
+  switch (operator) {
+    case '+':
+      return x + y;
+    case '-':
+      return x - y;
+    case '*':
+      return x * y;
+    default:
+      throw new Error(`There is no such operator like '${operator}'!`);
+  }
+};
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const getRandomIndex = (data) => Math.floor(Math.random() * data.length);
 
 const getQuestionAndAnswer = () => {
-	const question = getRandomExpression();
-	const correctAnswer = evaluate(question);
-	return [question, correctAnswer];
+  const number1 = getRandomNumber(minRange, maxRange);
+  const number2 = getRandomNumber(minRange, maxRange);
+  const operator = operators[getRandomIndex(operators)];
+  const question = `${number1} ${operator} ${number2}`;
+  const correctAnswer = calculate(number1, number2, operator).toString();
+  return [question, correctAnswer];
 };
 
 export const gameBrainCalc = () => {gameProcess(getQuestionAndAnswer,description)};
